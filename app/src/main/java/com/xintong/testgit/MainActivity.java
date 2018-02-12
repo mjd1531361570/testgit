f57941b6529afa953468ea2b209c2000200d39cf
mjd1531361570 <1531361570@qq.com>
2018-02-12 11:03:53 +0800

    第二次提交

--- app/src/main/java/com/xintong/testgit/MainActivity.java
+++ app/src/main/java/com/xintong/testgit/MainActivity.java
@@ -23,5 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //我是二的
+        int a=0;
     }
 }
