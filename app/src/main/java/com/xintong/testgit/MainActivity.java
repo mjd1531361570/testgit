22b626c955be2ddd0c4127ab4353657d670e34c7
mjd1531361570 <1531361570@qq.com>
2018-02-12 10:07:50 +0800

    哈哈哈443

--- app/src/main/java/com/xintong/testgit/MainActivity.java
+++ app/src/main/java/com/xintong/testgit/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 //sdfgdsfgfdsgfdgb
     ///冲突了哈哈1
+    ///冲突了哈哈2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
